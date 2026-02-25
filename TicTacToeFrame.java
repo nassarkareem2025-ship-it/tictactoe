e4bc05275ec90c3ed8dd83f1e048228865038fa9
nassarkareem2025-ship-it <nassarkareem2025@gmail.com>
2026-02-25 13:35:08 -0500

    Add files via upload

---
+++ TicTacToeFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TicTacToeFrame extends JFrame {
+
+    private TicTacToeTile[][] board = new TicTacToeTile[3][3];
+
+    public TicTacToeFrame(){
+
+
+        }
+}
